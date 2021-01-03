c18f44a524e4bd7c1c9e477e2a2caa76cfe08592
Kyle Mulqueen <kmulqdrums710@gmail.com>
2021-01-03 00:15:01 -0700

    Created route to get stick control exercise by id.

--- server/controllers/stickControlExerciseController.js
+++ server/controllers/stickControlExerciseController.js
@@ -6,7 +6,25 @@ module.exports = {
   getAllExercises: async function (req, res) {
     try {
       const exercises = await Exercise.find();
-      res.json(exercises);
+
+      if (exercises.length) {
+        res.json(exercises);
+      } else {
+        res.status(404).json({ msg: "No exercises found" });
+      }
+    } catch (error) {
+      res.status(422).json(error);
+    }
+  },
+  getExerciseByID: async function (req, res) {
+    try {
+      const exercise = await Exercise.findById(req.params.id);
+
+      if (exercise) {
+        res.json(exercise);
+      } else {
+        res.status(404).json({ msg: "Exercise not found." });
+      }
     } catch (error) {
       res.status(422).json(error);
     }
--- server/routes/api/stickControlExerciseRoutes.js
+++ server/routes/api/stickControlExerciseRoutes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const stickControlExerciseController = require("../../controllers/stickControlExerciseController");
 
+router.route("/:id").get(stickControlExerciseController.getExerciseByID);
 router.route("/").get(stickControlExerciseController.getAllExercises);
 
 module.exports = router;
