8bab94e599b5c84c81d3b29cc561cf37a8e97f5a
Kyle Mulqueen <kmulqdrums710@gmail.com>
2021-01-11 15:15:41 -0700

    setting up heroku to test on mobile.

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const path = require("path");
 const routes = require("./routes");
 const connectDB = require("./config/db");
 
@@ -9,6 +10,15 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
