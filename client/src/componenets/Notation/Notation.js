57c75f3896dbf460092a7597cf023e0b7a2be5b9
Kyle Mulqueen <kmulqdrums710@gmail.com>
2021-01-16 10:34:22 -0700

    Added notation class to all notation-... classes to clean up the notation scss file.

--- client/src/componenets/Notation/Notation.js
+++ client/src/componenets/Notation/Notation.js
@@ -71,213 +71,249 @@ const Notation = ({ rhythm }) => {
         switch (rhythm) {
           // Base Exercises
           case "1+2+3+4+|1+2+3+4+":
-            return <EighthNotes className="notation-sbc" />;
+            return <EighthNotes className="notation notation-sbc" />;
           case "F+aF+a|F+aF+a":
-            return <Flams className="notation-flams" />;
+            return <Flams className="notation notation-flams" />;
           case "Fe+aFe+a|Fe+aFe+a":
-            return <FlamsSixteenths className="notation-flams-16s" />;
+            return <FlamsSixteenths className="notation notation-flams-16s" />;
           case "Fe+aFe+a|FeFaFeFa":
             return (
-              <Flams16sFlamsTaps16s className="notation-flams-16s-flams-taps-16s" />
+              <Flams16sFlamsTaps16s className="notation notation-flams-16s-flams-taps-16s" />
             );
           case "FeFaFeFa|Fe+aFe+a":
             return (
-              <FlamsTaps16sFlams16s className="notation-flams-taps-16s-flams-16s" />
+              <FlamsTaps16sFlams16s className="notation notation-flams-taps-16s-flams-16s" />
             );
           case "FeFaFeFa|FeFaFeFa":
-            return <FlamsSixteenthsTaps className="notation-flams-taps-16s" />;
+            return (
+              <FlamsSixteenthsTaps className="notation notation-flams-taps-16s" />
+            );
           case "F+aF+a|Fe+aFe+a":
-            return <FlamsMixed className="notation-flams-flams-16s" />;
+            return <FlamsMixed className="notation notation-flams-flams-16s" />;
           case "F+aF+a|FeFaFeFa":
-            return <FlamsMixedTaps className="notation-flams-flams-taps-16s" />;
+            return (
+              <FlamsMixedTaps className="notation notation-flams-flams-taps-16s" />
+            );
 
           // === MIXES ===
           // 2/4 Time (1 Flam)
           case "Fe+aFe+a|F+aF+a":
             return (
-              <MixFlams16sFlams2 className="notation-mixed-flams-16s-flams" />
+              <MixFlams16sFlams2 className="notation notation-mixed-flams-16s-flams" />
             );
           case "FeFaFeFa|F+aF+a":
             return (
-              <MixFlamsTaps16sFlams2 className="notation-mixed-flams-taps-16s-flams" />
+              <MixFlamsTaps16sFlams2 className="notation notation-mixed-flams-taps-16s-flams" />
             );
 
           // 4/4 (1 SBC + 1 Flam)
           case "1+2+3+4+|F+aF+aF+aF+a":
-            return <MixSBCFlams4 className="notation-mixed-sbc-flams-4" />;
+            return (
+              <MixSBCFlams4 className="notation notation-mixed-sbc-flams-4" />
+            );
           case "1+2+3+4+|F+aF+aFe+aFe+a":
             return (
-              <MixSBCFlamsFlams16s4 className="notation-mixed-sbc-flams-flams-16s-4" />
+              <MixSBCFlamsFlams16s4 className="notation notation-mixed-sbc-flams-flams-16s-4" />
             );
           case "1+2+3+4+|F+aF+aFeFaFeFa":
             return (
-              <MixSBCFlamsFlamsTaps16s4 className="notation-mixed-sbc-flams-flams-taps-16s-4" />
+              <MixSBCFlamsFlamsTaps16s4 className="notation notation-mixed-sbc-flams-flams-taps-16s-4" />
             );
           case "1+2+3+4+|Fe+aFe+aFe+aFe+a":
             return (
-              <MixSBCFlams16s4 className="notation-mixed-sbc-flams-16s-4" />
+              <MixSBCFlams16s4 className="notation notation-mixed-sbc-flams-16s-4" />
             );
           case "1+2+3+4+|Fe+aFe+aFeFaFeFa":
             return (
-              <MixSBCFlams16sFlamsTaps16s4 className="notation-mixed-sbc-flams-16s-flams-taps-16s-4" />
+              <MixSBCFlams16sFlamsTaps16s4 className="notation notation-mixed-sbc-flams-16s-flams-taps-16s-4" />
             );
           case "1+2+3+4+|FeFaFeFaFe+aFe+a":
             return (
-              <MixSBCFlamsTaps16sFlams16s4 className="notation-mixed-sbc-flams-taps-16s-flams-16s-4" />
+              <MixSBCFlamsTaps16sFlams16s4 className="notation notation-mixed-sbc-flams-taps-16s-flams-16s-4" />
             );
 
           // 4/4 + 2/4 (1 SBC + 1 Flam Measure)
           case "1+2+3+4+|F+aF+a":
-            return <MixSBCFlams42 className="notation-mixed-sbc-flams-4-2" />;
+            return (
+              <MixSBCFlams42 className="notation notation-mixed-sbc-flams-4-2" />
+            );
           case "1+2+3+4+|Fe+aFe+a":
             return (
-              <MixSBCFlams16s42 className="notation-mixed-sbc-flams-16s-4-2" />
+              <MixSBCFlams16s42 className="notation notation-mixed-sbc-flams-16s-4-2" />
             );
           case "1+2+3+4+|FeFaFeFa":
             return (
-              <MixSBCFlamsTaps16s42 className="notation-mixed-sbc-flams-taps-16s-4-2" />
+              <MixSBCFlamsTaps16s42 className="notation notation-mixed-sbc-flams-taps-16s-4-2" />
             );
 
           // (1 Flam + 1 Flam Measure)
           case "F+aF+aF+aF+a|F+aF+a":
-            return <MixFlamsFlams42 className="notation-mixed" />;
+            return <MixFlamsFlams42 className="notation notation-mixed" />;
           case "F+aF+aF+aF+a|Fe+aFe+a":
-            return <MixFlamsFlams16s42 className="notation-mixed" />;
+            return <MixFlamsFlams16s42 className="notation notation-mixed" />;
           case "F+aF+aF+aF+a|FeFaFeFa":
-            return <MixFlamsFlamsTaps16s42 className="notation-mixed" />;
+            return (
+              <MixFlamsFlamsTaps16s42 className="notation notation-mixed" />
+            );
           case "F+aF+aFe+aFe+a|F+aF+a":
-            return <MixFlamsFlams16sFlams42 className="notation-mixed" />;
+            return (
+              <MixFlamsFlams16sFlams42 className="notation notation-mixed" />
+            );
           case "F+aF+aFe+aFe+a|Fe+aFe+a":
-            return <MixFlamsFlams16sFlams16s42 className="notation-mixed" />;
+            return (
+              <MixFlamsFlams16sFlams16s42 className="notation notation-mixed" />
+            );
           case "F+aF+aFe+aFe+a|FeFaFeFa":
             return (
-              <MixFlamsFlams16sFlamsTaps16s42 className="notation-mixed" />
+              <MixFlamsFlams16sFlamsTaps16s42 className="notation notation-mixed" />
             );
           case "F+aF+aFeFaFeFa|F+aF+a":
-            return <MixFlamsFlamsTaps16sFlams42 className="notation-mixed" />;
+            return (
+              <MixFlamsFlamsTaps16sFlams42 className="notation notation-mixed" />
+            );
           case "F+aF+aFeFaFeFa|Fe+aFe+a":
             return (
-              <MixFlamsFlamsTaps16sFlams16s42 className="notation-mixed" />
+              <MixFlamsFlamsTaps16sFlams16s42 className="notation notation-mixed" />
             );
           case "F+aF+aFeFaFeFa|FeFaFeFa":
             return (
-              <MixFlamsFlamsTaps16sFlamsTaps16s42 className="notation-mixed" />
+              <MixFlamsFlamsTaps16sFlamsTaps16s42 className="notation notation-mixed" />
             );
           case "Fe+aFe+aFe+aFe+a|F+aF+a":
-            return <MixFlams16sFlams42 className="notation-mixed" />;
+            return <MixFlams16sFlams42 className="notation notation-mixed" />;
           case "Fe+aFe+aFe+aFe+a|Fe+aFe+a":
-            return <MixFlams16sFlams16s42 className="notation-mixed" />;
+            return (
+              <MixFlams16sFlams16s42 className="notation notation-mixed" />
+            );
           case "Fe+aFe+aFe+aFe+a|FeFaFeFa":
-            return <MixFlams16sFlamsTaps16s42 className="notation-mixed" />;
+            return (
+              <MixFlams16sFlamsTaps16s42 className="notation notation-mixed" />
+            );
           case "Fe+aFe+aFeFaFeFa|F+aF+a":
             return (
-              <MixFlams16sFlamsTaps16sFlams42 className="notation-mixed" />
+              <MixFlams16sFlamsTaps16sFlams42 className="notation notation-mixed" />
             );
           case "Fe+aFe+aFeFaFeFa|Fe+aFe+a":
             return (
-              <MixFlams16sFlamsTaps16sFlams16s42 className="notation-mixed" />
+              <MixFlams16sFlamsTaps16sFlams16s42 className="notation notation-mixed" />
             );
           case "Fe+aFe+aFeFaFeFa|FeFaFeFa":
             return (
-              <MixFlams16sFlamsTaps16sFlamsTaps16s42 className="notation-mixed" />
+              <MixFlams16sFlamsTaps16sFlamsTaps16s42 className="notation notation-mixed" />
             );
           case "FeFaFeFaFe+aFe+a|F+aF+a":
             return (
-              <MixFlamsTaps16sFlams16sFlams42 className="notation-mixed" />
+              <MixFlamsTaps16sFlams16sFlams42 className="notation notation-mixed" />
             );
           case "FeFaFeFaFe+aFe+a|Fe+aFe+a":
             return (
-              <MixFlamsTaps16sFlams16sFlams16s42 className="notation-mixed" />
+              <MixFlamsTaps16sFlams16sFlams16s42 className="notation notation-mixed" />
             );
           case "FeFaFeFaFe+aFe+a|FeFaFeFa":
             return (
-              <MixFlamsTaps16sFlams16sFlamsTaps16s42 className="notation-mixed" />
+              <MixFlamsTaps16sFlams16sFlamsTaps16s42 className="notation notation-mixed" />
             );
           case "FeFaFeFaFeFaFeFa|F+aF+a":
             return (
-              <MixFlamsTaps16sFlamsTaps16sFlams42 className="notation-mixed" />
+              <MixFlamsTaps16sFlamsTaps16sFlams42 className="notation notation-mixed" />
             );
           case "FeFaFeFaFeFaFeFa|Fe+aFe+a":
             return (
-              <MixFlamsTaps16sFlamsTaps16sFlams16s42 className="notation-mixed" />
+              <MixFlamsTaps16sFlamsTaps16sFlams16s42 className="notation notation-mixed" />
             );
           case "FeFaFeFaFeFaFeFa|FeFaFeFa":
             return (
-              <MixFlamsTaps16sFlamsTaps16sFlamsTaps16s42 className="notation-mixed" />
+              <MixFlamsTaps16sFlamsTaps16sFlamsTaps16s42 className="notation notation-mixed" />
             );
 
           // 2/4 + 4/4 (1 Flam Measure + 1 SBC / Flam)
           case "F+aF+a|1+2+3+4+":
-            return <MixFlamsSBC24 className="notation-mixed-flams-sbc-2-4" />;
+            return (
+              <MixFlamsSBC24 className="notation notation-mixed-flams-sbc-2-4" />
+            );
           case "F+aF+a|F+aF+aF+aF+a":
-            return <MixFlamsFlams24 className="notation-mixed" />;
+            return <MixFlamsFlams24 className="notation notation-mixed" />;
           case "F+aF+a|F+aF+aFe+aFe+a":
-            return <MixFlamsFlamsFlams16s24 className="notation-mixed" />;
+            return (
+              <MixFlamsFlamsFlams16s24 className="notation notation-mixed" />
+            );
           case "F+aF+a|F+aF+aFeFaFeFa":
-            return <MixFlamsFlamsFlamsTaps16s24 className="notation-mixed" />;
+            return (
+              <MixFlamsFlamsFlamsTaps16s24 className="notation notation-mixed" />
+            );
           case "F+aF+a|Fe+aFe+aFe+aFe+a":
-            return <MixFlamsFlams16s24 className="notation-mixed" />;
+            return <MixFlamsFlams16s24 className="notation notation-mixed" />;
           case "F+aF+a|Fe+aFe+aFeFaFeFa":
             return (
-              <MixFlamsFlams16sFlamsTaps16s24 className="notation-mixed" />
+              <MixFlamsFlams16sFlamsTaps16s24 className="notation notation-mixed" />
             );
           case "F+aF+a|FeFaFeFaFe+aFe+a":
             return (
-              <MixFlamsFlamsTaps16sFlams16s24 className="notation-mixed" />
+              <MixFlamsFlamsTaps16sFlams16s24 className="notation notation-mixed" />
             );
           case "F+aF+a|FeFaFeFaFeFaFeFa":
-            return <MixFlamsFlamsTaps16s24 className="notation-mixed" />;
+            return (
+              <MixFlamsFlamsTaps16s24 className="notation notation-mixed" />
+            );
           case "Fe+aFe+a|1+2+3+4+":
             return (
-              <MixFlams16sSBC24 className="notation-mixed-flams-16s-sbc-2-4" />
+              <MixFlams16sSBC24 className="notation notation-mixed-flams-16s-sbc-2-4" />
             );
           case "Fe+aFe+a|F+aF+aF+aF+a":
-            return <MixFlams16sFlams24 className="notation-mixed" />;
+            return <MixFlams16sFlams24 className="notation notation-mixed" />;
           case "Fe+aFe+a|F+aF+aFe+aFe+a":
-            return <MixFlams16sFlamsFlams16s24 className="notation-mixed" />;
+            return (
+              <MixFlams16sFlamsFlams16s24 className="notation notation-mixed" />
+            );
           case "Fe+aFe+a|F+aF+aFeFaFeFa":
             return (
-              <MixFlams16sFlamsFlamsTaps16s24 className="notation-mixed" />
+              <MixFlams16sFlamsFlamsTaps16s24 className="notation notation-mixed" />
             );
           case "Fe+aFe+a|Fe+aFe+aFe+aFe+a":
-            return <MixFlams16s24 className="notation-mixed" />;
+            return <MixFlams16s24 className="notation notation-mixed" />;
           case "Fe+aFe+a|Fe+aFe+aFeFaFeFa":
             return (
-              <MixFlams16sFlams16sFlamsTaps16s24 className="notation-mixed" />
+              <MixFlams16sFlams16sFlamsTaps16s24 className="notation notation-mixed" />
             );
           case "Fe+aFe+a|FeFaFeFaFe+aFe+a":
             return (
-              <MixFlams16sFlamsTaps16sFlams16s24 className="notation-mixed" />
+              <MixFlams16sFlamsTaps16sFlams16s24 className="notation notation-mixed" />
             );
           case "Fe+aFe+a|FeFaFeFaFeFaFeFa":
-            return <MixFlams16sFlamsTaps16s24 className="notation-mixed" />;
+            return (
+              <MixFlams16sFlamsTaps16s24 className="notation notation-mixed" />
+            );
           case "FeFaFeFa|1+2+3+4+":
             return (
-              <MixFlamsTaps16sSBC24 className="notation-mixed-flams-taps-16s-sbc-2-4" />
+              <MixFlamsTaps16sSBC24 className="notation notation-mixed-flams-taps-16s-sbc-2-4" />
             );
           case "FeFaFeFa|F+aF+aF+aF+a":
-            return <MixFlamsTaps16sFlams24 className="notation-mixed" />;
+            return (
+              <MixFlamsTaps16sFlams24 className="notation notation-mixed" />
+            );
           case "FeFaFeFa|F+aF+aFe+aFe+a":
             return (
-              <MixFlamsTaps16sFlamsFlams16s24 className="notation-mixed" />
+              <MixFlamsTaps16sFlamsFlams16s24 className="notation notation-mixed" />
             );
           case "FeFaFeFa|F+aF+aFeFaFeFa":
             return (
-              <MixFlamsTaps16sFlamsFlamsTaps16s24 className="notation-mixed" />
+              <MixFlamsTaps16sFlamsFlamsTaps16s24 className="notation notation-mixed" />
             );
           case "FeFaFeFa|Fe+aFe+aFe+aFe+a":
-            return <MixFlamsTaps16sFlams16s24 className="notation-mixed" />;
+            return (
+              <MixFlamsTaps16sFlams16s24 className="notation notation-mixed" />
+            );
           case "FeFaFeFa|Fe+aFe+aFeFaFeFa":
             return (
-              <MixFlamsTaps16sFlams16sFlamsTaps16s24 className="notation-mixed" />
+              <MixFlamsTaps16sFlams16sFlamsTaps16s24 className="notation notation-mixed" />
             );
           case "FeFaFeFa|FeFaFeFaFe+aFe+a":
             return (
-              <MixFlamsTaps16sFlamsTaps16sFlams16s24 className="notation-mixed" />
+              <MixFlamsTaps16sFlamsTaps16sFlams16s24 className="notation notation-mixed" />
             );
           case "FeFaFeFa|FeFaFeFaFeFaFeFa":
             return (
-              <MixFlamsTaps16sFlamsTaps16sFlamsTaps16s24 className="notation-mixed" />
+              <MixFlamsTaps16sFlamsTaps16sFlamsTaps16s24 className="notation notation-mixed" />
             );
 
           default:
